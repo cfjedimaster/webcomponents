3126e3a468b65cc349e656ca3b8fb6c401553cd2
Raymond Camden <raymondcamden@gmail.com>
2023-01-20 08:28:50 -0600

    new demo

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -13,11 +13,19 @@ class SlideShow extends HTMLElement {
 			return;
 		}
 
+		if(!this.hasAttribute('width')) {
+			// default
+			this.setAttribute('width', 500);
+		}
+
 		/*
 		Convert attribute into an array and do some trimming so that the end user can have some spacing
 		*/
 		this.images = this.getAttribute('images').split(',').map(i => i.trim());
 		//console.log('images',images);
+		
+		// preload for quicker response, we don't need to wait for this
+		this.preload(this.images);
 
 		this.totalImages = this.images.length;
 
@@ -29,16 +37,27 @@ class SlideShow extends HTMLElement {
 		<img id="currentImage" src="${this.images[this.current]}">
 		<p>
 		<button id="prevButton">Previous</button> 
+		Picture <span id="currentPicture">1</span> of ${this.totalImages}
 		<button id="nextButton">Next</button> 
 		</p>
 		`;
 
 		this.$nextButton = wrapper.querySelector('#nextButton');
 		this.$prevButton = wrapper.querySelector('#prevButton');
+		this.$currentPicture = wrapper.querySelector('#currentPicture');
 		this.$image = wrapper.querySelector('#currentImage');
 		
-
+		const style = document.createElement('style');
+		style.innerHTML = `
+div {
+	width: ${this.getAttribute('width')}px
+}
+p {
+text-align: center;
+}
+		`;
 		shadow.appendChild(wrapper);
+		shadow.appendChild(style);
 		
 	}
 	connectedCallback() {
@@ -63,6 +82,14 @@ class SlideShow extends HTMLElement {
 		console.log('updateImage called');
 		console.log('current is', this.current);
 		this.$image.src = this.images[this.current];
+		this.$currentPicture.innerText = this.current+1;
+	}
+
+	preload(i) {
+		for(let x=0; x<i.length; x++) {
+			let img = new Image();
+			img.src = i[x];
+		}
 	}
 }
 
